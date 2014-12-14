7d7ae71c8f491aa7765642e9ee2fa10bf37899ed
Helder Silva <heldim.ufv@gmail.com>
2014-12-14 12:23:38 -0200

    Iniciando fase final do site

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,7 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
-$to = 'teixeiragastaguatinga@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'teixeiragastagua@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Contato pelo Site:  $name";
 $email_body = "Você recebeu uma nova mensagem pela página de contatos do site.\n\n"."Detalhes:\n\nNome: $name\n\nEmail: $email_address\n\nTelefone: $phone\n\nMensagem:\n$message";
 $headers = "From: noreply@teixeragas.com.br\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
